f7ead2985963d56219d27c2c9ae29399a4ff1f7b
thanapat.tst@gmail.com <thanapat.tst@gmail.com>
2023-11-29 07:44:15 +0000

    add 2

--- hello_world.r
+++ hello_world.r
@@ -2,4 +2,20 @@ print("hello world")
 
 print(1+2)
 
-mtcars
+f1 <- mtcars %>% 
+  filter(hp>300)
+
+df2 <- mtcars %>%
+  filter(hp <= 80)
+
+df3 <- mtcars %>%
+  filter(am == "Auto")
+
+df4 <- mtcars %>%
+  filter(mpg >= 20)
+
+full_df <- df1 %>%
+  bind_rows(df2) %>%
+  bind_rows(df3) %>%
+  bind_rows(df4)
+
